afa900ad7cef9f5064d914f077fed1fd60bd3683
coderblu <118417802+coderblu@users.noreply.github.com>
2024-02-28 22:59:23 +0530

    Update Pizza.java

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -1,34 +1,77 @@
 package com.driver;
 
 public class Pizza {
-
     private int price;
-    private Boolean isVeg;
-    private String bill;
+    private int basePrice; //price without extra additions
+    private Boolean isExtraCheese = false;
+    private Boolean isToppings = false;
+    private Boolean isTakeAway = false;
+    int takeAwayPrice = 50;
+    int extraCheesePrice = 100;
+    int extraToppingsPrice = 100;
 
     public Pizza(Boolean isVeg){
-        this.isVeg = isVeg;
-        // your code goes here
+        //true for a vegetable pizza
+        System.out.println("WELCOME TO BEST PIZZAS");
+        if(isVeg){
+            this.price = 300;
+        }else{
+            this.price = 350;
+        }
+        basePrice = price;
     }
 
-    public int getPrice(){
-        return this.price;
-    }
+    public int getBasePrice(){
+        return this.basePrice;
+    }//for future use
 
     public void addExtraCheese(){
-        // your code goes here
+        if(!isExtraCheese) {
+            System.out.println("+ Added extra cheese!: " + extraCheesePrice + " Ksh.");
+            this.price += extraCheesePrice;
+        }else{
+            System.out.println("!* Extra cheese already added!");
+        }
+        isExtraCheese = true;
     }
 
     public void addExtraToppings(){
-        // your code goes here
+        if(!isToppings) {
+            System.out.println("+ Added extra toppings!: " + extraToppingsPrice + " Ksh.");
+            this.price += extraToppingsPrice;
+        }else {
+            System.out.println("*! Extra toppings already added!");
+        }
+        isToppings = true;
     }
 
-    public void addTakeaway(){
-        // your code goes here
+    public void takeAway(){
+        if(!isTakeAway) {
+            System.out.println("+ Takeaway order for: " + takeAwayPrice + " Ksh.");
+            this.price += takeAwayPrice;
+        }else{
+            System.out.println("*! Takeaway already selected!");
+        }
+        isTakeAway = true;
     }
 
-    public String getBill(){
-        // your code goes here
-        return this.bill;
+    public void getBill(){
+        System.out.println();
+        System.out.println("----------------------------------");
+        System.out.println("-------------Bill-----------------");
+        System.out.println("Base pizza: " + basePrice + " Ksh.");
+        if(isExtraCheese){
+            System.out.println("Extra cheese: " + extraCheesePrice + " Ksh.");
+        }
+        if(isToppings){
+            System.out.println("Extra toppings: " + extraToppingsPrice + " Ksh.");
+        }
+        if(isTakeAway){
+            System.out.println("Takeaway services: " + takeAwayPrice + " Ksh.");
+        }
+        System.out.println("--------------TOTAL---------------");
+        System.out.println("\t \t \t Ksh: " + this.price);
+        System.out.println("----------------------------------");
+        System.out.println();
     }
 }
